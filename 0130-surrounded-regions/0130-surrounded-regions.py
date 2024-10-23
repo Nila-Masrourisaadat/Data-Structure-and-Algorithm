f06a2ee3f8e6cdeff947eec37f73094f455eadba
Nila MasrourSaadat <46789308+nila-masroori@users.noreply.github.com>
2024-10-23 16:59:07 -0600

    Time: 15 ms (96.9%), Space: 16 MB (97.4%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+
+        ROWS,COLS=len(board),len(board[0])
+        directions=[[1,0],[0,1],[-1,0],[0,-1]]
+
+        def dfs(row,col):
+            if row==ROWS or row<0 or col==COLS or col<0 or board[row][col]!="O":
+                return 
+            board[row][col]="T"
+            for dr, dc in directions:
+                # if board[row+dr][col+dc]=="O":
+                dfs(row+dr,col+dc)
+                    
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (board[row][col]=="O" and (row in [0,ROWS-1] or col in [0,COLS-1])):#unsurrounded regions
+                    # board[row][col]="T"
+                    dfs(row, col)
+
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col]=="O":
+                    board[row][col]="X"
+                if board[row][col]=="T":
+                    board[row][col]="O"
+        
