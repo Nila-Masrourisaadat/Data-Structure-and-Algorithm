d80696c965cd3e962fcf4a03238398e2c2e012c7
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-05 13:12:43 -0700

    Time: 4175 ms (5.01%), Space: 12.7 MB (75.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,17 +7,15 @@ class Solution(object):
       
         l,r=0,0
         while  l<len(nums):
-            while nums[l]!=0 and l<len(nums)-1:
-                l+=1
+            # while nums[l]!=0 and l<len(nums)-1:
+            #     l+=1
             #if l is pointing at zero, r should go until finding non zero starting from l
             r=l
             while nums[r]==0 and r<len(nums)-1:
                 r+=1
             #here l is pointing at zero and r has found next non zero
             #swap
-            tmp=nums[l]
-            nums[l]=nums[r]
-            nums[r]=tmp
+            nums[r],nums[l]=nums[l],nums[r]
             #after the swap we move l to where r is and go from there
             l+=1
         return nums
