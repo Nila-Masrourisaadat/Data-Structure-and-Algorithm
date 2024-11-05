a72528ef4680a3b1eae0496eca8afd5804cc6b08
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-05 12:59:50 -0700

    Time: 4278 ms (5.01%), Space: 12.7 MB (91.35%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)==1 and nums[0]!=0:
+            return nums
+        l,r=0,0
+        while  l<len(nums):
+            while nums[l]!=0 and l<len(nums)-1:
+                l+=1
+            #if l is pointing at zero, r should go until finding non zero starting from l
+            r=l
+            while nums[r]==0 and r<len(nums)-1:
+                r+=1
+            #here l is pointing at zero and r has found next non zero
+            #swap
+            tmp=nums[l]
+            nums[l]=nums[r]
+            nums[r]=tmp
+            #after the swap we move l to where r is and go from there
+            l+=1
+        return nums
