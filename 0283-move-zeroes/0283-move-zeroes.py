8dcde61a41a7c5e8d00d7641e09e6f21717768a0
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-05 13:14:41 -0700

    Time: 5 ms (73.72%), Space: 12.8 MB (49.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,17 +5,25 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
       
-        l,r=0,0
-        while  l<len(nums):
-            # while nums[l]!=0 and l<len(nums)-1:
-            #     l+=1
-            #if l is pointing at zero, r should go until finding non zero starting from l
-            r=l
-            while nums[r]==0 and r<len(nums)-1:
-                r+=1
-            #here l is pointing at zero and r has found next non zero
-            #swap
-            nums[r],nums[l]=nums[l],nums[r]
-            #after the swap we move l to where r is and go from there
-            l+=1
+        # l,r=0,0
+        # while  l<len(nums):
+        #     # while nums[l]!=0 and l<len(nums)-1:
+        #     #     l+=1
+        #     #if l is pointing at zero, r should go until finding non zero starting from l
+        #     r=l
+        #     while nums[r]==0 and r<len(nums)-1:
+        #         r+=1
+        #     #here l is pointing at zero and r has found next non zero
+        #     #swap
+        #     nums[r],nums[l]=nums[l],nums[r]
+        #     #after the swap we move l to where r is and go from there
+        #     l+=1
+        # return nums
+
+        #second solution
+        l=0
+        for r in range(len(nums)):
+            if nums[r]:#if r is pointing at non zero
+                nums[r],nums[l]=nums[l],nums[r]
+                l+=1
         return nums
