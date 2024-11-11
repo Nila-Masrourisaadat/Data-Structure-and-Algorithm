b417f6e2a7149ff5046133d3f22ba72719681b73
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-11 14:54:43 -0700

    Time: 95 ms (57.19%), Space: 17.7 MB (66.52%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -5,9 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        # #O(nlogn)
+        # distance=[]
+        # for i,(x,y) in enumerate(points):
+        #     distance.append((x**2+y**2,i))
+        # #sort based on the first element of the tuples in the array distance
+        # distance.sort(key=lambda x:x[0] )
+        # return [points[dis[1]] for dis in distance[:k]]
+
+        #o(nlogn)but without sort function
         distance=[]
+        res=[]
         for i,(x,y) in enumerate(points):
-            distance.append((x**2+y**2,i))
-        #sort based on the first element of the tuples in the array distance
-        distance.sort(key=lambda x:x[0] )
-        return [points[dis[1]] for dis in distance[:k]]
+            heapq.heappush(distance,(x**2+y**2,i))
+        for i in range(k):
+            dis=heapq.heappop(distance)[1]
+            res.append(points[dis])
+        return res
