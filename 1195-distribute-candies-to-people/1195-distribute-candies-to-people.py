25e6560f420d6714fcec162c9514b6712d135c69
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-12 12:04:08 -0700

    Time: 6 ms (36.67%), Space: 11.8 MB (19.4%) - LeetHub

--- 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
+++ 1195-distribute-candies-to-people/1195-distribute-candies-to-people.py
@@ -5,22 +5,33 @@ class Solution(object):
         :type num_people: int
         :rtype: List[int]
         """
-        output=[0]*num_people
-        i=0
-        round=0
-        while candies:
-            if i==0:
-                addition=round*num_people+1
-            if candies>=addition+i:
-                output[i]+=addition+i
-                candies-=addition+i
-            else:
-                break
-            if i<num_people-1:
-                i+=1
-            else:
-                round+=1
-                i=0
+        # output=[0]*num_people
+        # i=0
+        # round=0
+        # while candies:
+        #     if i==0:
+        #         addition=round*num_people+1
+        #     if candies>=addition+i:
+        #         output[i]+=addition+i
+        #         candies-=addition+i
+        #     else:
+        #         break
+        #     if i<num_people-1:
+        #         i+=1
+        #     else:
+        #         round+=1
+        #         i=0
             
-        output[i]+=candies
+        # output[i]+=candies
+        # return output
+
+        candy=0
+        output=[0]*num_people
+        while candies>0:
+            for i in range(num_people):
+                candy+=1
+                output[i]+=min(candy,candies)
+                candies-=candy
+                if candies<0:
+                    break
         return output
