328c4b5111c839271a3dd18a7663aaa5c6de4cdd
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-13 18:35:33 -0700

    Time: 0 ms (100%), Space: 11.8 MB (38.55%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,33 +4,51 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        N=len(nums)
-        B=[0]*len(nums)
-        bitnums=[]
-        def generatebitnums(i,B,bitnums):
-            if i==N:
-                bitnums.append(B[:])
-                return
+        # N=len(nums)
+        # B=[0]*len(nums)
+        # bitnums=[]
+        # def generatebitnums(i,B,bitnums):
+        #     if i==N:
+        #         bitnums.append(B[:])
+        #         return
             
-            #for including the element
-            B[i]=1
-            generatebitnums(i+1,B,bitnums)
+        #     #for including the element
+        #     B[i]=1
+        #     generatebitnums(i+1,B,bitnums)
 
-            #for not including the element
-            B[i]=0
-            generatebitnums(i+1,B,bitnums)
+        #     #for not including the element
+        #     B[i]=0
+        #     generatebitnums(i+1,B,bitnums)
 
         
         
 
-        generatebitnums(0,B,bitnums)
-        result=[]
-        for nums2 in bitnums:
-            subset=[]
-            for i,num in enumerate(nums2):
-                if num==1:
-                    subset.append(nums[i])
-            result.append(subset)
+        # generatebitnums(0,B,bitnums)
+        # result=[]
+        # for nums2 in bitnums:
+        #     subset=[]
+        #     for i,num in enumerate(nums2):
+        #         if num==1:
+        #             subset.append(nums[i])
+        #     result.append(subset)
         
-        return result
+        # return result
             
+
+        #another shorter solution but same kinda algo or logic
+        subset=[]
+        res=[]
+        def dfs(i):
+            if i>=len(nums):
+                res.append(subset[:])
+                return 
+
+            #including that element
+            subset.append(nums[i]) 
+            dfs(i+1)
+
+            #not including that element
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
