0f8dd13412fe229e67d3f4d4516ef68e62bc9796
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-14 08:24:42 -0700

    Time: 211 ms (44.21%), Space: 13.2 MB (60.28%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -16,15 +16,15 @@ class Solution(object):
             heapq.heappush(h, -counter[key])#negated because its minheap
 
         while q or h:
-            time+=1
             
+            if q and q[0][1]==time:
+                cnt,time=q.popleft()#remember cnt is negative
+                heapq.heappush(h,cnt)
+            time+=1
             if h:
                 cnt=heapq.heappop(h)
                 if cnt+1<0:
                     q.append((cnt+1,time+n))
-            if q and q[0][1]==time:
-                cnt,time=q.popleft()#remember cnt is negative
-                heapq.heappush(h,cnt)
             
         return time
                 
