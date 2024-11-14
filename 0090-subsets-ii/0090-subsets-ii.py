9e31910604f8ad70fd58409141a203fd43ff6a88
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-14 15:03:59 -0700

    Time: 10 ms (5.92%), Space: 11.9 MB (20.26%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # res=[]
+        # subset=[]
+        # def dfs(i):
+        #     if i>=len(nums):
+        #         subset.sort()
+        #         if subset not in res:
+        #             res.append(subset[:])
+        #         return
+
+        #     subset.append(nums[i])
+        #     dfs(i+1)
+
+        #     subset.pop()
+        #     dfs(i+1)
+        
+        # dfs(0)
+        # return res
+
+
+        def generatebits(i,B,Bits):
+            if i==len(nums):
+                Bits.append(B[:])
+                return
+            
+            B[i]=0
+            generatebits(i+1,B,Bits)
+
+            B[i]=1
+            generatebits(i+1,B,Bits)
+
+        B=[0]*len(nums)
+        Bits=[]
+        res=[]
+        generatebits(0,B,Bits)
+
+        for B in Bits:
+            subset=[]
+            for i, b in enumerate(B):
+                if b==1:
+                    subset.append(nums[i])
+            subset.sort()
+            if subset not in res:
+                res.append(subset)
+        return res
+            
