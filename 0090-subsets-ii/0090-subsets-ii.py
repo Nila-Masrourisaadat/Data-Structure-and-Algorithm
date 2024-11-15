175440b905dce643dead07a13bac6a141339c572
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-14 18:09:10 -0700

    Time: 0 ms (100%), Space: 11.9 MB (20.26%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,48 +4,49 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # res=[]
-        # subset=[]
-        # def dfs(i):
-        #     if i>=len(nums):
-        #         subset.sort()
-        #         if subset not in res:
-        #             res.append(subset[:])
-        #         return
-
-        #     subset.append(nums[i])
-        #     dfs(i+1)
-
-        #     subset.pop()
-        #     dfs(i+1)
-        
-        # dfs(0)
-        # return res
-
-        nums.sort()
-        def generatebits(i,B,Bits):
-            if i==len(nums):
-                Bits.append(B[:])
-                return
-            
-            B[i]=0
-            generatebits(i+1,B,Bits)
-
-            B[i]=1
-            generatebits(i+1,B,Bits)
-
-        B=[0]*len(nums)
-        Bits=[]
         res=[]
-        generatebits(0,B,Bits)
+        nums.sort()
+        def dfs(i,subset):
+            if i==len(nums):
+                res.append(subset[:])
+                return
 
-        for B in Bits:
-            subset=[]
-            for i, b in enumerate(B):
-                if b==1:
-                    subset.append(nums[i])
-            # subset.sort()
-            if subset not in res:
-                res.append(subset)
+            subset.append(nums[i])
+            dfs(i+1,subset)
+
+            subset.pop()
+
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
+            dfs(i+1,subset)
+        
+        dfs(0,[])
         return res
+
+        # nums.sort()
+        # def generatebits(i,B,Bits):
+        #     if i==len(nums):
+        #         Bits.append(B[:])
+        #         return
+            
+        #     B[i]=0
+        #     generatebits(i+1,B,Bits)
+
+        #     B[i]=1
+        #     generatebits(i+1,B,Bits)
+
+        # B=[0]*len(nums)
+        # Bits=[]
+        # res=[]
+        # generatebits(0,B,Bits)
+
+        # for B in Bits:
+        #     subset=[]
+        #     for i, b in enumerate(B):
+        #         if b==1:
+        #             subset.append(nums[i])
+        #     # subset.sort()
+        #     if subset not in res:
+        #         res.append(subset)
+        # return res
             
