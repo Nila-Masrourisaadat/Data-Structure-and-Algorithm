31977bad276bd58a6c255ad7007ee2d4cebd7aad
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-17 16:06:44 -0700

    Time: 52 ms (24.02%), Space: 11.7 MB (62.6%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -20,7 +20,7 @@ class Solution(object):
 
             #unchoose
             candidate=subset.pop()
-            while i+1<len(candidates) and candidates[i]==candidates[i+1]:
+            while i+1<len(candidates) and candidate==candidates[i+1]:
                 i+=1
             dfs(i+1,subset,sum)
             
