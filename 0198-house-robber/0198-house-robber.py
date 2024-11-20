0ad7726ff2e5c97bffafe97b9ca22208adea321f
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-20 11:40:30 -0700

    Time: 0 ms (100%), Space: 11.7 MB (9.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -6,16 +6,23 @@ class Solution(object):
         """
         if len(nums)==1:
             return nums[0]
-        dp={}
-        dp[0]=nums[0]
+        # dp={}
+        # dp[0]=nums[0]
       
         
-        def HR(i):
-            if i==1:
-                dp[1]= max(nums[0],nums[1])
-                return dp[1]
-            if i in dp:
-                return dp[i]
-            dp[i]=max(HR(i-2)+nums[i],HR(i-1))
-            return dp[i]
-        return HR(len(nums)-1)
+        # def HR(i):
+        #     if i==1:
+        #         dp[1]= max(nums[0],nums[1])
+        #         return dp[1]
+        #     if i in dp:
+        #         return dp[i]
+        #     dp[i]=max(HR(i-2)+nums[i],HR(i-1))
+        #     return dp[i]
+        # return HR(len(nums)-1)
+
+        first=nums[0]
+        second=max(first,nums[1])
+        for i in range(2,len(nums)):
+            first,second=second,max(second, first+nums[i])
+        return second
+            
