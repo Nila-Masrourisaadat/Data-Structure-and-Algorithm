ce6371a3319204b653be9b15cc62f7f4b46e4b2c
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-22 15:18:00 -0700

    Time: 55 ms (5.29%), Space: 11.9 MB (37.76%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -7,34 +7,35 @@ class Solution(object):
         :rtype: bool
         """
         #bottom up-iterative
-        # dp=[ [False]*len(s1) for _ in range(len(s2)) ]
-        # dp[len(s1)][:]=True
-        # dp[len(s2)][:]=True
+        if len(s1)+len(s2)!=len(s3):
+            return False
 
-        # for i in range(len(s1)-1,-1,-1):
-        #     for j in range(len(s2)-1,-1,-1):
-        #         if s1[i]==s3[i+j] and (dp[i+1][j] or dp[i][j+1]):
-        #             dp[i][j]=True
-        #         if s2[j]==s3[i+j] and (dp[i+1][j] or dp[i][j+1]):
-        #             dp[i][j]=True
-                
-        #         return False
-        # return dp[0][0]
+        dp=[ [False]*(len(s2)+1) for _ in range(len(s1)+1) ]
+        print(dp)
+        dp[len(s1)][len(s2)]=True
+
+        for i in range(len(s1),-1,-1):
+            for j in range(len(s2),-1,-1):
+                if i<len(s1) and i+j<len(s3) and s1[i]==s3[i+j] and dp[i+1][j]:
+                    dp[i][j]=True
+                if j<len(s2) and i+j<len(s3) and s2[j]==s3[i+j] and dp[i][j+1]:
+                    dp[i][j]=True   
+        return dp[0][0]
 
         #memoization and recursive
-        dp={}
-        def dfs(i,j):
-            if i==len(s1) and j==len(s2):
-                return i + j == len(s3)
-            if (i,j) in dp:
-                return dp[(i,j)]
-            if i<len(s1)  and i+j<len(s3) and s1[i]==s3[i+j] and dfs(i+1,j):
-                # dp[(i,j)]=True
-                return True
-            if j<len(s2) and i+j<len(s3) and s2[j]==s3[i+j] and dfs(i,j+1):
-                return True
-                # dp[(i,j)]=True #we dont have to cache cuz of this being dfs and going till the end of a path if one of the path is True then we return true and that's enough however we have to cache when its false so we dont go down that route again 
-            dp[(i,j)]=False
-            return False
+        # dp={}
+        # def dfs(i,j):
+        #     if i==len(s1) and j==len(s2):
+        #         return i + j == len(s3)#check if we have reached the end of s3 or not 
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+        #     if i<len(s1)  and i+j<len(s3) and s1[i]==s3[i+j] and dfs(i+1,j):
+        #         # dp[(i,j)]=True
+        #         return True
+        #     if j<len(s2) and i+j<len(s3) and s2[j]==s3[i+j] and dfs(i,j+1):
+        #         return True
+        #         # dp[(i,j)]=True #we dont have to cache cuz of this being dfs and going till the end of a path if one of the path is True then we return true and that's enough however we have to cache when its false so we dont go down that route again 
+        #     dp[(i,j)]=False
+        #     return False
         
-        return dfs(0,0)
+        # return dfs(0,0)
