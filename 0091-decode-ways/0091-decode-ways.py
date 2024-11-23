0e4e9cc160eb53c3ba279f33e738025c261b1983
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-22 21:53:55 -0700

    Time: 4 ms (27.92%), Space: 12 MB (18.95%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,23 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
+        #bottom up
         dp={len(s):1}
         
+        # for i in range(len(s)-1,-1,-1):
+        #     if s[i]=="0":
+        #         dp[i]=0
+        #     else:
+        #         dp[i]=dp[i+1]
+        #     if i<len(s)-1 and int(s[i]+s[i+1])<27 and s[i]!="0":
+        #         dp[i]+=dp[i+2]
+            
+        # return dp[0]
 
-        for i in range(len(s)-1,-1,-1):
+        #memoization
+        def dfs(i):
+            if i in dp:
+                return dp[i]
             if s[i]=="0":
                 dp[i]=0
-            #itself
-            else:
-                dp[i]=dp[i+1]
-            #one before
+                return 0
+            dp[i]=dfs(i+1)
             if i<len(s)-1 and int(s[i]+s[i+1])<27 and s[i]!="0":
-                dp[i]+=dp[i+2]
-            
-        return dp[0]
+                dp[i]+=dfs(i+2)
+            return dp[i]
 
-
+        return dfs(0)
 
                 
 
