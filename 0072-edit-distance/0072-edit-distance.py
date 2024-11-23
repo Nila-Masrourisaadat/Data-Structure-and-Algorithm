cbf7831a5b7dbcfc602d6080aa42404ece1c0299
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-22 18:03:21 -0700

    Time: 80 ms (56.22%), Space: 15 MB (58.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -6,32 +6,32 @@ class Solution(object):
         :rtype: int
         """
         #memoization and recursive
-        dp = {}
+        # dp = {}
 
-        def dfs(i, j):
-            # Base cases
-            if i == len(word1):  # If word1 is exhausted, insert remaining characters of word2
-                return len(word2) - j
-            if j == len(word2):  # If word2 is exhausted, delete remaining characters of word1
-                return len(word1) - i
+        # def dfs(i, j):
+        #     # Base cases
+        #     if i == len(word1):  # If word1 is exhausted, insert remaining characters of word2
+        #         return len(word2) - j
+        #     if j == len(word2):  # If word2 is exhausted, delete remaining characters of word1
+        #         return len(word1) - i
             
-            if (i, j) in dp:  # Check memoization table
-                return dp[(i, j)]
+        #     if (i, j) in dp:  # Check memoization table
+        #         return dp[(i, j)]
 
-            # If characters match, move to the next characters in both words
-            if word1[i] == word2[j]:
-                dp[(i, j)] = dfs(i + 1, j + 1)
-            else:
-                # If characters don't match, consider the three possible operations
-                insert = dfs(i, j + 1)    # Insert a character in word1
-                delete = dfs(i + 1, j)    # Delete a character from word1
-                replace = dfs(i + 1, j + 1)  # Replace a character in word1
-                dp[(i, j)] = 1 + min(insert, delete, replace)  # Take the minimum cost
+        #     # If characters match, move to the next characters in both words
+        #     if word1[i] == word2[j]:
+        #         dp[(i, j)] = dfs(i + 1, j + 1)
+        #     else:
+        #         # If characters don't match, consider the three possible operations
+        #         insert = dfs(i, j + 1)    # Insert a character in word1
+        #         delete = dfs(i + 1, j)    # Delete a character from word1
+        #         replace = dfs(i + 1, j + 1)  # Replace a character in word1
+        #         dp[(i, j)] = 1 + min(insert, delete, replace)  # Take the minimum cost
 
-            return dp[(i, j)]
+        #     return dp[(i, j)]
 
-        # Call the function starting from the beginning of both words
-        return dfs(0, 0)
+        # # Call the function starting from the beginning of both words
+        # return dfs(0, 0)
 
 
         #bottom up and iterative
