d9ea383b787d3ee347b6202f5fe470f2d6548f72
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-22 21:54:07 -0700

    Time: 0 ms (100%), Space: 11.8 MB (23.56%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,15 +7,15 @@ class Solution(object):
         #bottom up
         dp={len(s):1}
         
-        # for i in range(len(s)-1,-1,-1):
-        #     if s[i]=="0":
-        #         dp[i]=0
-        #     else:
-        #         dp[i]=dp[i+1]
-        #     if i<len(s)-1 and int(s[i]+s[i+1])<27 and s[i]!="0":
-        #         dp[i]+=dp[i+2]
+        for i in range(len(s)-1,-1,-1):
+            if s[i]=="0":
+                dp[i]=0
+            else:
+                dp[i]=dp[i+1]
+            if i<len(s)-1 and int(s[i]+s[i+1])<27 and s[i]!="0":
+                dp[i]+=dp[i+2]
             
-        # return dp[0]
+        return dp[0]
 
         #memoization
         def dfs(i):
