d948adae86580769ce30182ac16e22a1b678dca8
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-11-24 21:15:53 -0700

    Time: 0 ms (100%), Space: 11.6 MB (42.13%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -19,3 +19,33 @@ class Solution(object):
         res=[]
         dfs(root,res)
         return res
+
+         #recursive
+        # res=[]
+        # def inorder(root):
+        #     if not root:
+        #         return
+        #     inorder(root.left)
+        #     res.append(root.val)
+        #     inorder(root.right)
+        
+        # inorder(root)
+        # return res
+
+        #iterative
+        stack=[]
+        res=[]
+        node=root
+        while node or stack:
+            while node:
+                stack.append(node)
+                node=node.left
+            node=stack.pop()
+            res.append(node.val)
+            node=node.right
+             
+        return res
+            
+#why is iterative faster thank recursive?
+
+
