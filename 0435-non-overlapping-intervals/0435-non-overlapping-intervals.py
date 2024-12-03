7493050f0201c89c9516d421a7156a7250d74c93
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-12-03 13:02:52 -0700

    Time: 207 ms (24.04%), Space: 45.2 MB (28.2%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -4,12 +4,25 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: int
         """
+        #better
+        intervals.sort()
+        cnt=0
+        prev_end=intervals[0][1]
+        for start, end in intervals[1:]:
+            if start>=prev_end:
+                #non overlapping
+                prev_end=end
+            else: #overlapping
+                cnt+=1
+                prev_end=min(prev_end, end)
+        return cnt
+
+        #mine:
         sorted_intervals=sorted(intervals, key=lambda x:x[0])
         prev=sorted_intervals[0]
         cnt=0
         for i,(next_s, next_e)in enumerate(sorted_intervals[1:]):
             start,end=prev[0],prev[1]
-            print([start,end],[next_s,next_e])
             #check if they overlap
             if start<=next_s<end or start<next_e<=end:
                 if end<next_e or (end==next_e and start>next_s):
