4d557a6170396305cca411c5481b0542b337f3c5
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-12-04 16:36:30 -0700

    Time: 3 ms (80.93%), Space: 12.9 MB (7.43%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,17 +4,46 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+        #better with O(1) space complexity
         ROWS,COLS=len(matrix),len(matrix[0])
-        rows,cols=set(),set()
+        FirstRow=False
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c]==0:
-                    rows.add(r)
-                    cols.add(c) 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if r in rows or c in cols:
+                    matrix[0][c]=0
+                    if r>0:
+                        matrix[r][0]=0
+                    else:
+                        FirstRow=True
+        for r in range(1,ROWS):
+            for c in range(1,COLS):
+                if matrix[0][c]==0 or matrix[r][0]==0:
                     matrix[r][c]=0
+        
+        #first column seperately
+        if matrix[0][0]==0:# this condition is enough because if in column 0 anything was zero matrix[0][0] would have changed to zero
+            for r in range(ROWS):
+                matrix[r][0]=0
+        #first row seperately
+        if FirstRow:
+            for c in range(COLS):
+                matrix[0][c]=0
+
+
+
+        
+        #mine with O(m+n) space complexity
+        # ROWS,COLS=len(matrix),len(matrix[0])
+        # rows,cols=set(),set()
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if matrix[r][c]==0:
+        #             rows.add(r)
+        #             cols.add(c) 
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if r in rows or c in cols:
+        #             matrix[r][c]=0
 
         
         
