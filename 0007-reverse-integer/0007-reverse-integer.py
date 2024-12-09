ea5144cc27b40e4d113f74cff3ed537eddbb5b89
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2024-12-09 10:47:13 -0700

    Time: 14 ms (73.88%), Space: 12.2 MB (9.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -15,7 +15,6 @@ class Solution(object):
             cnt+=1
             if x:
                 if (not flag and res>(2**31-1)//10) or (flag and res>2**31//10):
-                    print(flag,2**31//10,res,x)
                     return 0
                 elif (not flag and res==(2**31-1)//10) or (flag and res==2**31//10):
                     if (not flag and x>7) or (flag and x>8):
