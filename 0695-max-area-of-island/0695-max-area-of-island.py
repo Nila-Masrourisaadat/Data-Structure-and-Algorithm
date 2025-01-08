4c679aec66d707a8c221256399bf8a9e62f00c9f
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-01-07 20:37:05 -0700

    Time: 170 ms (5.05%), Space: 12.8 MB (74.93%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,37 +4,45 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        def bfs(m,n,visit):
-            q=collections.deque()
-            q.append((m,n))
-            visit.add((m,n))
-            cnt=1
-            while q:
-                row,col=q.pop()
-                # if (row,col) in visit:
-                #     continue
-                # visit.add((row,col))
-                for dr,dc in directions:
-                    r=row+dr
-                    c=col+dc
-                    if r==ROWS or c==COLS or r<0 or c<0 or grid[r][c]==0 or (r,c) in visit:
-                        continue
-                    
-                    visit.add((r,c))
-                    cnt+=1
-                    print(r,c,cnt)
-                    q.append((r,c))
-            return cnt
-
-
-
         ROWS,COLS=len(grid),len(grid[0])
+        directions=[[1,0],[0,1],[-1,0],[0,-1]]
         visit=set()
-        res=0
-        directions=[[-1,0],[1,0],[0,-1],[0,1]]
-        for m in range(ROWS):
-            for n in range (COLS):
-                if grid[m][n]==1:
-                    res=max(res,bfs(m,n,visit))
-        
-        return res
+        max_area=0
+        area=0
+
+        def bfs(r,c):
+            print(r,c)
+            q=collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+            area=1
+            while q:
+                row,col=q.popleft()
+                # print("here2",row,col)
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    # if (row,col)==(4,9):
+                    #     if (r,c) in visit:
+                    #         print("in visit",row,col,r,c)
+                    #     if grid[r][c]==1:
+                    #         print("its land")
+                    #     if r not in range(ROWS) or c not in range(COLS):
+                    #         # print("not in bound")
+                    
+                    if r in range(ROWS) and c in range(COLS) and (r,c) not in visit and grid[r][c]==1:
+                        # print("here",r,c)
+                        # print(visit)
+                        #its land that is part of an island
+                        area+=1
+                        q.append((r,c))
+                        visit.add((r,c))
+            # print(area)
+            return area
+
+
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if (i,j) not in visit and grid[i][j]==1:
+                    max_area=max(bfs(i,j),max_area)
+        return max_area
