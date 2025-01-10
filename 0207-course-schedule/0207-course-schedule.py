7a5fc60dc37a2b6635932bc019263c25358df0be
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-01-09 21:41:04 -0700

    Time: 9 ms (61.13%), Space: 16.4 MB (5.23%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        adj=collections.defaultdict(list)
+        visit, cycle=set(),set()
+        for ai,bi in prerequisites:
+            adj[bi].append(ai)
+        
+        def dfs(node):
+            if node in cycle:
+                return False
+            if node in visit:
+                return True
+            cycle.add(node)
+            for child in adj[node]:
+                if not dfs(child):
+                    return False
+            cycle.remove(node)
+            visit.add(node)
+            return True
+        
+        for n in range(numCourses):
+            if not dfs(n):
+                return False
+        return True
