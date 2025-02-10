fb8c36f7afb7d2822aa042c63d6d96e7791309f8
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-10 12:11:25 -0700

    Time: 8 ms (69.19%), Space: 13.6 MB (16.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,32 +4,46 @@ class Solution(object):
         :type isConnected: List[List[int]]
         :rtype: int
         """
+        # BFS
         provinces=0
         ROWS,COLS=len(isConnected), len(isConnected[0])
-        Adj=collections.defaultdict(list)
+        # Adj=collections.defaultdict(list)
         visit=set()
-        q=collections.deque()
-        for i in range(ROWS):
-            for j in range(i,COLS): 
-                if isConnected[i][j]==1:
-                    Adj[i].append(j)
-                    Adj[j].append(i)
-        def bfs(row,col):
-            q.append(row)
+        # q=collections.deque()
+        # for i in range(ROWS):
+        #     for j in range(i,COLS): 
+        #         if isConnected[i][j]==1:
+        #             Adj[i].append(j)
+        #             Adj[j].append(i)
+        # def bfs(row,col):
+        #     q.append(row)
 
-            while q:
-                row=q.popleft()
+        #     while q:
+        #         row=q.popleft()
                 
-                for city in Adj[row]:
-                    if city not in visit:
-                        q.append(city)
-                        visit.add(city)
+        #         for city in Adj[row]:
+        #             if city not in visit:
+        #                 q.append(city)
+        #                 visit.add(city)
 
+
+        # for i in range(ROWS):
+        #     for j in range(i,COLS): 
+        #         if i not in visit:
+        #             bfs(i,j)
+        #             provinces+=1
+        # return provinces if len(visit)==ROWS else provinces+ROWS-len(visit)
+
+        #DFS
+        def dfs(i):
+            visit.add(i)
+            for j in range(COLS):
+                if isConnected[i][j]==1 and j not in visit:
+                    dfs(j)
 
         for i in range(ROWS):
-            for j in range(i,COLS): 
-                if i not in visit:
-                    bfs(i,j)
-                    provinces+=1
-        print(visit)
-        return provinces if len(visit)==ROWS else provinces+ROWS-len(visit)
+            if i not in visit:
+                dfs(i)
+                provinces+=1
+
+        return provinces
