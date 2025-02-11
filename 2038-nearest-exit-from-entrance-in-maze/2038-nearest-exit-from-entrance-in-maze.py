6acce615d5368e152b4a7acf268203e1194a6757
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-11 10:44:58 -0700

    Time: 367 ms (13.7%), Space: 14.9 MB (46.58%) - LeetHub

---
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def nearestExit(self, maze, entrance):
+        """
+        :type maze: List[List[str]]
+        :type entrance: List[int]
+        :rtype: int
+        """
+        
+        ROWS,COLS=len(maze),len(maze[0])
+        directions=[[0,1],[1,0],[-1,0],[0,-1]]
+        q=collections.deque()
+        visit=set()
+        steps=0
+
+        q.append((entrance[0],entrance[1]))
+        visit.add((entrance[0],entrance[1]))
+        while q:
+            steps+=1
+            for _ in range(len(q)):
+                row,col=q.popleft()
+
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    if (r,c) not in visit and r in range(0,ROWS) and c in range(0,COLS) and maze[r][c]==".":
+                        if r==0 or c==0 or r==ROWS-1 or c==COLS-1: #border
+                            return steps
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        if ((row==0 or row==ROWS-1) and row!=entrance[0]) or ((col==0 or col==COLS-1) and col!=entrance[1]):
+            
+            return steps
+        
+        return -1
