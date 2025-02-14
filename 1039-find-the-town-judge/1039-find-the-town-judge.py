4f68be21c93520de3c0c431a0545cdd4a8ba3a91
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-14 11:05:31 -0700

    Time: 447 ms (5.35%), Space: 16.1 MB (29.41%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -17,10 +17,8 @@ class Solution(object):
                 potential_judge=i
         print(Adj)
         if potential_judge==0:
-            print("here")
             return -1
         for truster,trustee in Adj.items():
             if potential_judge not in Adj[truster]:
-                print("here2")
                 return -1
         return potential_judge
