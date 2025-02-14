67acdd15f4e709c7809ba3087febf3d1d98d8ed8
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-14 16:10:20 -0700

    Time: 20 ms (51.86%), Space: 16 MB (63.54%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -5,20 +5,33 @@ class Solution(object):
         :type trust: List[List[int]]
         :rtype: int
         """
-        if not trust and n>1:
-            return -1
-        Adj=collections.defaultdict(list)
-        potential_judge=0
-        for (truster, trustee) in trust:
-            Adj[truster].append(trustee)
+        incoming=defaultdict(int)
+        outgoing=defaultdict(int)
+        
+        for truster, trustee in trust:
+            outgoing[truster]=outgoing[truster]+1
+            incoming[trustee]=incoming[trustee]+1
         for i in range(1,n+1):
-            print(i)
-            if i not in Adj:
-                potential_judge=i
-        print(Adj)
-        if potential_judge==0:
-            return -1
-        for truster,trustee in Adj.items():
-            if potential_judge not in Adj[truster]:
-                return -1
-        return potential_judge
+            if incoming[i]==n-1 and outgoing[i]==0:
+                return i
+        return -1
+
+
+
+        # if not trust and n>1:
+        #     return -1
+        # Adj=collections.defaultdict(list)
+        # potential_judge=0
+        # for (truster, trustee) in trust:
+        #     Adj[truster].append(trustee)
+        # for i in range(1,n+1):
+        #     print(i)
+        #     if i not in Adj:
+        #         potential_judge=i
+        # print(Adj)
+        # if potential_judge==0:
+        #     return -1
+        # for truster,trustee in Adj.items():
+        #     if potential_judge not in Adj[truster]:
+        #         return -1
+        # return potential_judge
