8eee28177bf43fb1f45701e8fa0f1a73a432fd98
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-18 12:58:01 -0700

    Time: 47 ms (77.98%), Space: 18.5 MB (21.55%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type graph: List[List[int]]
         :rtype: List[int]
         """
-        loop=set()
         dp={}
         def dfs(node):
             #dp/memoization to avoid repeated searches and optimization
@@ -12,16 +11,11 @@ class Solution(object):
                 return dp[node]
             if node in terminal:#when we find out that this path we are in is leading to terminal node
                 return True
-            #cycle detection    
-            if node in loop:
-                return False
-            loop.add(node)
-            ####
+            dp[node]=False
             for child in graph[node]:
                 if not dfs(child):#if only a path from only even one of the children doesnt lead to terminal then its False
                     dp[node]=False
                     return False
-            loop.remove(node)# for cycle detection, have to remove the node from loop set after the dfs is complete
             dp[node]=True
             return True
         #finding the terminal nodes
@@ -36,3 +30,40 @@ class Solution(object):
                 res.append(i)
 
         return res
+
+
+
+
+
+        # loop=set()
+        # dp={}
+        # def dfs(node):
+        #     #dp/memoization to avoid repeated searches and optimization
+        #     if node in dp:
+        #         return dp[node]
+        #     if node in terminal:#when we find out that this path we are in is leading to terminal node
+        #         return True
+        #     #cycle detection    
+        #     if node in loop:
+        #         return False
+        #     loop.add(node)
+        #     ####
+        #     for child in graph[node]:
+        #         if not dfs(child):#if only a path from only even one of the children doesnt lead to terminal then its False
+        #             dp[node]=False
+        #             return False
+        #     loop.remove(node)# for cycle detection, have to remove the node from loop set after the dfs is complete
+        #     dp[node]=True
+        #     return True
+        # #finding the terminal nodes
+        # terminal=set()
+        # for i in range(len(graph)):
+        #     if not graph[i]:
+        #         terminal.add(i)
+        # #finding the safe nodes
+        # res=[]
+        # for i in range(len(graph)):
+        #     if dfs(i):
+        #         res.append(i)
+
+        # return res
