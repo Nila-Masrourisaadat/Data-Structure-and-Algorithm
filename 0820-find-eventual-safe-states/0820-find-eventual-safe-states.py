f5dbba6b356f63a600a75889206e8e537c333596
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-18 12:33:14 -0700

    Time: 59 ms (58.14%), Space: 19.1 MB (5.9%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -7,26 +7,29 @@ class Solution(object):
         loop=set()
         dp={}
         def dfs(node):
-
+            #dp/memoization to avoid repeated searches and optimization
             if node in dp:
                 return dp[node]
-            if node in terminal:
+            if node in terminal:#when we find out that this path we are in is leading to terminal node
                 return True
+            #cycle detection    
             if node in loop:
                 return False
             loop.add(node)
+            ####
             for child in graph[node]:
-                if not dfs(child):
+                if not dfs(child):#if only a path from only even one of the children doesnt lead to terminal then its False
                     dp[node]=False
                     return False
-            loop.remove(node)
+            loop.remove(node)# for cycle detection, have to remove the node from loop set after the dfs is complete
             dp[node]=True
             return True
-
+        #finding the terminal nodes
         terminal=set()
         for i in range(len(graph)):
             if not graph[i]:
                 terminal.add(i)
+        #finding the safe nodes
         res=[]
         for i in range(len(graph)):
             if dfs(i):
