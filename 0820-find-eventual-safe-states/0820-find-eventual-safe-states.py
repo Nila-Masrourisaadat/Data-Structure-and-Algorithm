35737727e9ad2e5ae3ee25840720b96185731631
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-02-18 13:01:28 -0700

    Time: 51 ms (69.23%), Space: 18.3 MB (39.25%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -9,8 +9,6 @@ class Solution(object):
             #dp/memoization to avoid repeated searches and optimization
             if node in dp:
                 return dp[node]
-            if node in terminal:#when we find out that this path we are in is leading to terminal node
-                return True
             dp[node]=False
             for child in graph[node]:
                 if not dfs(child):#if only a path from only even one of the children doesnt lead to terminal then its False
@@ -18,11 +16,7 @@ class Solution(object):
                     return False
             dp[node]=True
             return True
-        #finding the terminal nodes
-        terminal=set()
-        for i in range(len(graph)):
-            if not graph[i]:
-                terminal.add(i)
+        
         #finding the safe nodes
         res=[]
         for i in range(len(graph)):
@@ -30,8 +24,6 @@ class Solution(object):
                 res.append(i)
 
         return res
-
-
 
 
 
