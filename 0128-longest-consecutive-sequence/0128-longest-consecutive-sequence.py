f5664c2c4fea5ac9ab17177691e148044f241363
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-03 15:06:30 -0700

    Time: 45 ms (88.51%), Space: 27.1 MB (46.96%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,22 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #input: unsorted array int of nums
-        #output: length of the longest consecutive elements sequence
-        #time complexity o(n)
-        #what if we create an array with size of the max(nums) and run through nums and put the numbers in the indexes of array and run through that new array one more time and everytime we encounter inconsistency thats not the sequence, the longest we can go without inconsistency is the answer but this is low key giving sorting so why not sort but sort is O(nlogn) but what im saying is bucket sort
+        num_set = set(nums)
+        longest = 0
 
-        #hashset or hashmap to group them
-        visit=set(nums)
-        cnt=0
-        res=0
-        for num in nums:
-            if num-1 not in visit:#only if it start of a sequence
-                cnt+=1
-                while num+1 in visit:
-                    cnt+=1
-                    num+=1
-            res=max(cnt,res)
-            cnt=0
-        return res
-           
+        for n in num_set:
+            if n - 1 not in num_set:
+                length = 1
+
+                while n + length in num_set:
+                    length += 1
+                
+                longest = max(longest, length)
+        
+        return longest
