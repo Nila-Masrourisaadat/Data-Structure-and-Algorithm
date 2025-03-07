f25d08fed63e23cd3d97307b1f825f90afdedb5b
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-07 09:24:02 -0700

    Time: 1916 ms (31.75%), Space: 20.5 MB (5.39%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,18 +5,30 @@ class Solution(object):
         :rtype: str
         """
         
-     
-        resIdx, resLen = 0, 0
-        n = len(s)
+        res=[[False] *len(s) for _ in range(len(s))]
+        resLen,resIdx=0,0
+        for i in range(len(s)-1,-1,-1):
+            for j in range(i,len(s)):
+                if s[i] == s[j] and (j - i <= 2 or res[i + 1][j - 1]):
+                    res[i][j]=True
+                    if resLen<(j-i+1):
+                        resLen= j-i+1
+                        resIdx=i
+        return s[resIdx:resIdx+resLen]
 
-        dp = [[False] * n for _ in range(n)]
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
-                    dp[i][j] = True
-                    if resLen < (j - i + 1):
-                        resIdx = i
-                        resLen = j - i + 1
+   
+        # resIdx, resLen = 0, 0
+        # n = len(s)
 
-        return s[resIdx : resIdx + resLen]
+        # dp = [[False] * n for _ in range(n)]
+
+        # for i in range(n - 1, -1, -1):
+        #     for j in range(i, n):
+        #         if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+        #             dp[i][j] = True
+        #             if resLen < (j - i + 1):
+        #                 resIdx = i
+        #                 resLen = j - i + 1
+
+        # return s[resIdx : resIdx + resLen]
