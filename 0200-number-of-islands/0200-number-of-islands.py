d5f21fa73bc3a642dcf1de0052a1e2b5c330c4f6
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-07 06:42:26 -0700

    Time: 1496 ms (12.34%), Space: 31.3 MB (30.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,26 +4,31 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        visit=set()
-        directions=[[0,1],[1,0],[0,-1],[-1,0]]
-        ROW,COL=len(grid),len(grid[0])
-        islands=0
-        def bfs(r,c):
-            q=collections.deque()
-            visit.add((r,c))
-            q.append((r,c))
+        ROWS,COLS=len(grid),len(grid[0])
+        directions=[[1,0],[0,1],[-1,0],[0,-1]]
 
+        q=collections.deque()
+        islands=0
+        visit=set()
+
+        def bfs(row,col):
+            q.append((row,col))
+            visit.add((row,col))
             while q:
                 row,col=q.popleft()
                 
-                for dr, dc in directions:
+                for dr,dc in directions:
                     r,c=row+dr,col+dc
-                    if (r,c) not in visit and 0<=r<ROW and 0<=c<COL and grid[r][c]=="1":
+                    if r in range(ROWS) and c in range(COLS) and (r,c) not in visit and grid[r][c]=="1":
                         q.append((r,c))
                         visit.add((r,c))
-        for i in range(ROW):
-            for j in range(COL):
-                if (i,j) not in visit and grid[i][j]=="1":
-                    bfs(i,j)
+
+
+
+
+        for row in range(ROWS):
+            for col in range(COLS):
+                if grid[row][col]=="1" and (row,col) not in visit:
+                    bfs(row,col)
                     islands+=1
         return islands
