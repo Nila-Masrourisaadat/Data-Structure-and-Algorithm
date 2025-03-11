a8b0e9b41d3b7f6819e74cfc2ead11d03af6321f
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-11 16:18:50 -0600

    Time: 0 ms (100%), Space: 12.4 MB (54.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,10 +4,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp={}
-        dp[0]=dp[1]=1
-        for i in range(2, n+1):
-            if i in dp:
-                return dp[i]
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
+        
         return dp[n]
+
+
+
+        def steps(n):
+            if n==0 or n==1:
+                return 1
+            return steps(n-1)+steps(n-2)
