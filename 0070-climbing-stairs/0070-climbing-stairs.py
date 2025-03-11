862c71e2b6fa71b578bebba58bec559c66e6f2b1
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-11 16:52:37 -0600

    Time: 0 ms (100%), Space: 12.6 MB (13.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,17 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp=[0]*(n+1)
-        dp[0]=1
-        dp[1]=1
-        for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
+        #bottom up recursive
+        # dp=[0]*(n+1)
+        # dp[0]=1
+        # dp[1]=1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
         
-        return dp[n]
+        # return dp[n]
 
-
-
-        def steps(n):
-            if n==0 or n==1:
-                return 1
-            return steps(n-1)+steps(n-2)
+        #top-down memoization
+        dp=[-1]*n
+        def steps(i):
+            if i>=n:
+                return i==n
+            if dp[i]!=-1:
+                return dp[i]
+            dp[i]=steps(i+1)+steps(i+2)
+            return dp[i]
+        return steps(0)
