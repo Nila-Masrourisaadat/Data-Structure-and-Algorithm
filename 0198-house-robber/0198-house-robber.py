af085ba74eb78d91551e7a30e327f95e967709c8
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-12 16:21:24 -0600

    Time: 0 ms (100%), Space: 12.3 MB (83.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
         rob1,rob2=0,0
 
         for n in nums:
@@ -11,7 +12,7 @@ class Solution(object):
             rob1=rob2
             rob2=temp
         return rob2
-        
+
         nums.append(0)
         for i in range(len(nums)-3,-1,-1):
             nums[i]=max(nums[i]+nums[i+2],nums[i+1])
