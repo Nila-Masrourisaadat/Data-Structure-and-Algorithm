8652ad1244c16968574afc783305129ae8a43a43
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-12 16:19:15 -0600

    Time: 0 ms (100%), Space: 12.4 MB (55.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        rob1,rob2=0,0
+
+        for n in nums:
+            temp=max(rob1+n,rob2)
+            rob1=rob2
+            rob2=temp
+        return rob2
+        
         nums.append(0)
         for i in range(len(nums)-3,-1,-1):
             nums[i]=max(nums[i]+nums[i+2],nums[i+1])
         return max(nums[0],nums[1])
+
