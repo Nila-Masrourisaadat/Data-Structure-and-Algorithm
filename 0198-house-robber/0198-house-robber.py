eaf8c3ed09bc82cb9ffa86b2d583775520f0fbed
Nila Masrourisaadat <46789308+Nila-Masrourisaadat@users.noreply.github.com>
2025-03-12 15:33:39 -0600

    Time: 0 ms (100%), Space: 12.6 MB (15.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,27 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)==1:
-            return nums[0]
-        # dp={}
-        # dp[0]=nums[0]
-      
-        #recusrive memoization
-        # def HR(i):
-        #     if i==1:
-        #         dp[1]= max(nums[0],nums[1])
-        #         return dp[1]
-        #     if i in dp:
-        #         return dp[i]
-        #     dp[i]=max(HR(i-2)+nums[i],HR(i-1))
-        #     return dp[i]
-        # return HR(len(nums)-1)
-
-
-        #iterative bottom up
-        first=nums[0]
-        second=max(first,nums[1])
-        for i in range(2,len(nums)):
-            first,second=second,max(second, first+nums[i])
-        return second
-            
+        nums.append(0)
+        for i in range(len(nums)-3,-1,-1):
+            nums[i]=max(nums[i]+nums[i+2],nums[i+1])
+        return max(nums[0],nums[1])
